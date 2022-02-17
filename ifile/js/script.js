80d83951a0e75b0674e976f30ad36176937605e8
Nishadchowdhury <Nishadhj111@gmail.com>
2022-02-17 19:50:36 +0600

    it's final commit more glitch fixed

--- ifile/js/script.js
+++ ifile/js/script.js
@@ -1,10 +1,6 @@
-
-
-// getting value
 function getvalue(input,check){
     let getInput = document.getElementById(input);
     const getInputN = parseFloat(getInput.value);
-
    if(check == true ){
        if(!isNaN(getInputN)){
          return parseFloat(getInput.value);
@@ -19,9 +15,7 @@ function getvalue(input,check){
    }else if((!isNaN(getInputN)) && (check == false)){
     return getInputN;    
    }
-   
 }
-
 //getting sum
 function sum(){
     let sumOf = 0;
@@ -30,20 +24,16 @@ function sum(){
     }
     return sumOf;
 }
-
 // get and replase value of textHolder 
 function replaseText(inputX, newValue){
     const getElement = document.getElementById(inputX);
     getElement.innerText = newValue;
 }
-
-
 document.getElementById('calculate-btn').addEventListener('click' , function(){
 const totalIncomeN = getvalue("Income-input" , false);
 const foodSpent = getvalue('Food-input', true);
 const rentSpant = getvalue('Rent-input', true);
 const clothesSpent = getvalue('Clothes-input', true);
-
 
 const totalExpenses = sum(foodSpent , rentSpant , clothesSpent);
 const FinalBalance = totalIncomeN - totalExpenses;
@@ -51,9 +41,7 @@ const FinalBalance = totalIncomeN - totalExpenses;
 if(totalIncomeN < 0){
     alert("plese , don't put Negative value ")
 }
-
 if(FinalBalance < 0 && FinalBalance != " "){
-   
     document.getElementById('Food-input').value = '';
     document.getElementById('Rent-input').value = '';
     document.getElementById('Clothes-input').value = ''; 
@@ -62,11 +50,8 @@ if(FinalBalance < 0 && FinalBalance != " "){
 //replace final vlaue
 replaseText("Total-Expenses", totalExpenses);
 replaseText("Total-Balance", FinalBalance);
-
 }
-
 //save section started
-
     if(totalIncomeN == '' || totalIncomeN > 0){
         
     document.getElementById('save-btn').addEventListener('click',function(){
@@ -76,20 +61,13 @@ replaseText("Total-Balance", FinalBalance);
     
     const saveoutput = document.getElementById('Total-saveing');
     const remeiningBalance = document.getElementById('Total-remaining');
-
     if(remeining < 0){
         alert("you don't have enough money , to save .")
     }else if(percent < 0){
         alert("plese , don't put Negative number to calculate saving")
     }
-    else{
-        
+    else{   
     remeiningBalance.innerText = remeining;
     saveoutput.innerText = percent;
     }})}
 });
-
-
-
-
-//function replaseText(inputX, newValue)
